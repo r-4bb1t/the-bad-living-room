a2400253b59b45de1755182029732050f87404de
r4bb1t <r4bb1t.github@gmail.com>
2024-05-29 18:19:01 +0900

    feat: main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,68 @@
 "use client";
 
-import { Button } from "@r-4bb1t/rabbit-ui";
+import { useState } from "react";
+
+import { Button, List, Select } from "@r-4bb1t/rabbit-ui";
 
 export default function Home() {
+  const hour = new Date().getHours();
+
+  const [startTime, setStartTime] = useState(hour);
+  const [endTime, setEndTime] = useState(Math.min(hour + 6, 23));
+
   return (
-    <main className="pt-24 pb-12 px-8 min-h-screen flex flex-col items-center justify-between">
-      <div className="italic text-lg">
-        여기는 <span className="text-primary-background">노넥</span>이 초대한
-        의상한거실...
-      </div>
-      <div className="p-4 rounded border border-primary-background border-dotted">
-        <h2 className="font-bold text-primary-background mb-1">Notice</h2>
-        <ul className="list-disc px-4 marker:text-primary-background leading-7">
+    <main className="pt-24 pb-12 px-8 min-h-screen flex flex-col items-center justify-between gap-4">
+      <div className="italic text-lg">여기는 의상한거실...</div>
+      <div className="p-4 rounded border border-primary border-dotted">
+        <h2 className="font-bold text-primary mb-1">Setting</h2>
+        <ul className="list-disc px-4 marker:text-primary leading-7">
           <li>
-            게임은 <span className="text-primary-background"></span>
+            게임은{" "}
+            <Select
+              sz={"xs"}
+              options={new Array(24).fill(0).map((_, i) => ({
+                value: i,
+                label: `${i}시`,
+              }))}
+              onChange={setStartTime}
+              value={startTime}
+              className="w-22 mr-2 text-primary"
+            />
             부터 시작됩니다.
           </li>
           <li>
             게임 종료 시간은{" "}
-            <span className="text-primary-background">
-              오후 5시 30분에서 6시 사이
+            <span className="text-primary">
+              <Select
+                sz={"xs"}
+                options={new Array(24).fill(0).map((_, i) => ({
+                  value: i,
+                  label: `${i}시`,
+                }))}
+                onChange={setEndTime}
+                value={endTime}
+                className="w-22 mr-2"
+              />
+              에서 {endTime}시 30분 사이
             </span>
-            에 <span className="text-primary-background">무작위</span>로
-            결정됩니다.
+            에 <span className="text-primary">무작위</span>로 결정됩니다.
           </li>
         </ul>
       </div>
-      <div className="p-4 rounded border border-primary-background border-dotted">
-        <h2 className="font-bold text-primary-background mb-1">Rule</h2>
-        <ol className="list-decimal px-4 marker:text-primary-background leading-7">
+      <div className="p-4 rounded border border-primary border-dotted">
+        <h2 className="font-bold text-primary mb-1">Rule</h2>
+        <List
+          type="decimal"
+          className="list-decimal px-4 marker:text-primary leading-7"
+        >
           <li>1시간마다 각자의 우편함에 쓰레기가 들어옵니다.</li>
           <li>
             우편함에 들어온것은{" "}
-            <span className="text-primary-background">우편함 확인하기</span>를
-            해야만 우편함에서 인벤토리로 넘어오며 확인이 가능합니다.
+            <span className="text-primary">우편함 확인하기</span>를 해야만
+            우편함에서 인벤토리로 넘어오며 확인이 가능합니다.
             <div className="text-sm font-light">
-              <span className="text-primary-background">*</span> 우편함
-              확인하기는 15분마다 가능합니다.
+              <span className="text-primary">*</span> 우편함 확인하기는 15분마다
+              가능합니다.
             </div>
           </li>
           <li>우편함에 들어온 지 30분이 지난 쓰레기는 귀속됩니다.</li>
@@ -46,12 +72,10 @@ export default function Home() {
           </li>
           <li>
             게임이 무작위로 종료되었을 때{" "}
-            <span className="text-primary-background">
-              가장 많은 쓰레기를 보유한 사람
-            </span>
+            <span className="text-primary">가장 많은 쓰레기를 보유한 사람</span>
             이 패배합니다.
           </li>
-        </ol>
+        </List>
       </div>
       <Button sz={"lg"} className="w-full">
         방 만들기
